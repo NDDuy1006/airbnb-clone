bd01b98f426ced5962fd1c3382130af43adb019b
Duy ND <duynd1006@gmail.com>
2023-08-28 23:55:18 +0700

    next commit

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -1,36 +1,27 @@
 "use client"
 
+import useSearchModal from "@/app/hooks/useSearchModal";
 import { BiSearch } from "react-icons/bi";
 
 const Search = () => {
+  const searchModal = useSearchModal();
+
   return (
     <div
       className="search-wrapper"
+      onClick={searchModal.onOpen}
     >
-      <div
-        className="flex flex-row items-center justif-between"
-      >
-        <div
-          className="text-sm font-semibold px-6"
-        >
+      <div className="flex flex-row items-center justif-between">
+        <div className="text-sm font-semibold px-6">
         Any Week
         </div>
-        <div
-          className="search-location"
-        >
+        <div className="search-location">
           Any where
         </div>
-        <div
-          className="search-guest"
-        >
+        <div className="search-guest">
           <div className="hidden sm:block">Add Guests</div>
           <div
-            className="
-              p-2
-              bg-rose-500
-              rounded-full
-              text-white
-            "
+            className="p-2 bg-rose-500 rounded-full text-white"
           >
             <BiSearch size={18} />
           </div>
